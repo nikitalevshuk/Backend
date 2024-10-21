53f15faa2c5cc6c27760cb7253487213919e0285
nikitalevshuk <levshukn@gmail.com>
2024-10-21 22:36:10 +0300

    Added specialists in models

--- app/models.py
+++ app/models.py
@@ -2,3 +2,8 @@ services = {
     1:{"id": 1, "name": "Стрижка", "description":"Классическая короткая стрижка", "price": 40, "duration": 90},
     2:{"id": 2, "name": "Бритье", "description":"Услуга бритья опасной бритвой", "price": 15, "duration": 30}
 }
+
+specialists = {
+    1:{"id": 1, "name": "Петька Гвоздиков"},
+    2:{"id": 2, "name": "Стас Шило"}
+}
