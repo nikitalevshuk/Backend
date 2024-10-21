252e9b3e82684bbac7dcd1d942d4ed4c8a18d6c9
nikitalevshuk <levshukn@gmail.com>
2024-10-21 22:24:20 +0300

    Created operation for reading of one service

--- app/main.py
+++ app/main.py
@@ -11,6 +11,9 @@ app = FastAPI()
 async def read_services():
     return list(services.values())
 
-
-
-
+#читаем одну услугу
+@app.get("/services/{service_id}", response_model=Service)
+async def read_service(service_id: Annotated[int, Path(description="The identification number of service")]):
+    if service_id not in services:
+        raise HTTPException(status_code=404, detail="Service not found")
+    return services[service_id]
