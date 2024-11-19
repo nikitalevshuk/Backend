62f1ae697435d48b0e64a81c71c3772fb6a888e1
nikitalevshuk <levshukn@gmail.com>
2024-11-19 17:37:55 +0300

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.operations_with_services import router as services_router 
 from app.operations_with_specialists import router as specialists_router
@@ -7,9 +7,16 @@ from app.middleware import RateLimitMiddleware, SizeLimitMiddleware
 
 app = FastAPI()
 
-# Подключаем обработчики ошибок
 app.add_middleware(RateLimitMiddleware, max_requests = 10, window_seconds = 60)
 app.add_middleware(SizeLimitMiddleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_methods=['GET'],
+    allow_headers=[],
+    max_age = 600,
+)
+
 
 # Подключаем роутеры
 app.include_router(services_router)
